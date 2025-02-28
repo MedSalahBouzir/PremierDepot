cc46d4080c15c4ce652e2ff4c3b3aa71dc0801ce
MedSalahBouzir <Salah.bouzir11@gmail.com>
2025-02-28 07:07:57 +0100

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,3 +1,3 @@
 public class Helloworld {
-  public static void main (string[] args){
-    system.out.println("Hello world");}}
+  public static void main (String[] args){
+    System.out.println("Hello world");}}
